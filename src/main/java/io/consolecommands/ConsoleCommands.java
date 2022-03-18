051fb71c5f88bc47e7dc887d8e8e196a240c05ad
Man297 <64731507+Man297@users.noreply.github.com>
2022-03-18 20:45:21 +0800

    Add bStats

--- src/main/java/io/consolecommands/ConsoleCommands.java
+++ src/main/java/io/consolecommands/ConsoleCommands.java
@@ -4,6 +4,7 @@ import io.consolecommands.commands.ccreload;
 import io.consolecommands.commands.executeconsole;
 import io.consolecommands.commands.executeplayer;
 import io.consolecommands.commands.loopcmd;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ConsoleCommands extends JavaPlugin {
@@ -20,6 +21,8 @@ public final class ConsoleCommands extends JavaPlugin {
         getCommand("loopcmd").setExecutor(new loopcmd());
         getCommand("loopcmd").setTabCompleter(new loopcmd());
         getCommand("ccreload").setExecutor(new ccreload());
+        int pluginId = Integer.parseInt("14662");
+        Metrics metrics = new Metrics(this, pluginId);
     }
 
     @Override
